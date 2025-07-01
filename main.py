68934fce7cdd5e3232e3b23e6de09759048820ee
Jeroen van der Heijden <jeroen@cesbit.com>
2025-07-01 11:34:28 +0200

    type

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from libprobe.probe import Probe
-from lib.check.device import check_device
+from lib.check.wireless import check_wireless
+from lib.check.memory import check_memory
+from lib.check.packet import check_packet
+from lib.check.connection import check_connection
 from lib.check.bss import check_bss
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
     checks = {
-        'device': check_device,
+        'wireless': check_wireless,
+        'memory': check_memory,
+        'packet': check_packet,
+        'connection': check_connection,
         'bss': check_bss,
     }
 
